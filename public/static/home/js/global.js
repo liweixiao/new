3fa2abdee36cc7656e03d6efe5421f6504d0ad60
liweixiao <719277529@qq.com>
2020-08-25 12:01:27 +0800

    提交文件

--- public/static/home/js/global.js
+++ public/static/home/js/global.js
@@ -280,6 +280,17 @@ go.calcTotalAmount = function (num, price) {
     return res;
 }
 
+//手机端展示menu
+go.showMenu = function (dom) {
+    $('.left-box').slideDown(200);
+    $(document).bind("click",function(e){
+            var target = $(e.target);
+            if(target.closest(".mobile-menu").length == 0 && target.closest(".left-box").length == 0 ){//点击id为parentId之外的地方触发
+                $('.left-box').slideUp(200);
+            }
+    })
+}
+
 
 
 
